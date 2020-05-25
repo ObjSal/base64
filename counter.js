ca6822968a3d9fc420713afc325663e8da0c0751
Salvador Guerrero <ObjSal@users.noreply.github.com>
2020-05-24 21:21:23 -0600

    Add counter test

---
+++ counter.js
@@ -0,0 +1,27 @@
+// Author: Salvador Guerrero
+
+'use strict'
+
+let footerDiv = document.getElementById('footer')
+
+function getCount() {
+  const urlParams = new URLSearchParams(window.location.search)
+  if (!urlParams) return
+  const counterParam = urlParams.get('counter')
+  if (!counterParam) return
+  fetch('localhost:3000/counter').then(response => {
+    if (response.status !== 200) return
+    (async() => {
+      try {
+        const text = await response.text()
+        footerDiv.textContent = `${footerDiv.textContent} | Visits: ${text}`
+      } catch (error) {
+        console.error(error)
+      }
+    })()
+  }).catch(reason => {
+    console.error(reason)
+  })
+}
+
+getCount()
