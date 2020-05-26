d1dc7618aecb2fb86a35b41420bf014b52694958
Salvador Guerrero <ObjSal@users.noreply.github.com>
2020-05-25 20:07:45 -0600

    Update counter path

--- counter.js
+++ counter.js
@@ -9,7 +9,7 @@ function getCount() {
   if (!urlParams) return
   const counterParam = urlParams.get('counter')
   if (!counterParam) return
-  fetch('http://localhost:3000/counter').then(response => {
+  fetch('http://localhost:3000/analytics/counter/base64').then(response => {
     if (response.status !== 200) return
     (async() => {
       try {
