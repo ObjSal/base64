fac1eaec922f9d625f79cf3e662c0730bcb4662b
Salvador Guerrero <ObjSal@users.noreply.github.com>
2020-05-27 23:19:50 -0600

    Disable counter

--- counter.js
+++ counter.js
@@ -5,10 +5,10 @@
 let footerDiv = document.getElementById('footer')
 
 function getCount() {
-  // const urlParams = new URLSearchParams(window.location.search)
-  // if (!urlParams) return
-  // const counterParam = urlParams.get('counter')
-  // if (!counterParam) return
+  const urlParams = new URLSearchParams(window.location.search)
+  if (!urlParams) return
+  const counterParam = urlParams.get('counter')
+  if (!counterParam) return
   fetch('http://byteapps.sytes.net:3000/analytics/counter/base64').then(response => {
     if (response.status !== 200) return
     (async() => {
